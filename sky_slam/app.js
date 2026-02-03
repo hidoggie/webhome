1339b1611cfee0991581fc7ce9923c7496d4233d
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-03 15:32:47 +0900

    Update app.js

--- sky_slam/app.js
+++ sky_slam/app.js
@@ -1,17 +1,18 @@
-import {recenterComponent} from './components/recenter.js''
+import {recenterComponent} from './components/recenter.js'
 AFRAME.registerComponent('recenter', recenterComponent)
 
 import {skyRemoteAuthoringComponent} from './components/sky-remote-authoring.js'
 AFRAME.registerComponent('sky-remote-authoring', skyRemoteAuthoringComponent)
 
-import {animateBalloonComponent} from './components/animate-balloon.js''
+import {animateBalloonComponent} from './components/animate-balloon.js'
 AFRAME.registerComponent('animate-balloon', animateBalloonComponent)
 
-import {transitionSceneComponent} from './components/transition-scene.js''
+import {transitionSceneComponent} from './components/transition-scene.js'
 AFRAME.registerComponent('transition-scene', transitionSceneComponent)
 
-import {hideOverlayComponent} from './components/hide-overlay.js''
+import {hideOverlayComponent} from './components/hide-overlay.js'
 AFRAME.registerComponent('hide-overlay', hideOverlayComponent)
 
 import {deviceOrientationComponent} from './components/device-orientation.js'
+
 AFRAME.registerComponent('device-orientation', deviceOrientationComponent)
