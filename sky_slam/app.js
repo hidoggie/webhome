ad2957c5563b2aa7cf43aaf98b70b7699e2167df
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-03 15:28:08 +0900

    Add files via upload

--- sky_slam/app.js
+++ sky_slam/app.js
@@ -1,18 +1,16 @@
-// Copyright (c) 2023 8th Wall, Inc.
-
-import {recenterComponent} from './components/recenter'
+import {recenterComponent} from './components/recenter.js''
 AFRAME.registerComponent('recenter', recenterComponent)
 
-import {skyRemoteAuthoringComponent} from './components/sky-remote-authoring'
+import {skyRemoteAuthoringComponent} from './components/sky-remote-authoring.js'
 AFRAME.registerComponent('sky-remote-authoring', skyRemoteAuthoringComponent)
 
-import {animateBalloonComponent} from './components/animate-balloon'
+import {animateBalloonComponent} from './components/animate-balloon.js''
 AFRAME.registerComponent('animate-balloon', animateBalloonComponent)
 
-import {transitionSceneComponent} from './components/transition-scene'
+import {transitionSceneComponent} from './components/transition-scene.js''
 AFRAME.registerComponent('transition-scene', transitionSceneComponent)
 
-import {hideOverlayComponent} from './components/hide-overlay'
+import {hideOverlayComponent} from './components/hide-overlay.js''
 AFRAME.registerComponent('hide-overlay', hideOverlayComponent)
 
 import {deviceOrientationComponent} from './components/device-orientation.js'
