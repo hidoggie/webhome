b439002b8dcdf120ca2c29fb5e457170231682e2
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-03 15:01:54 +0900

    Add files via upload

--- sky_slam/app.js
+++ sky_slam/app.js
@@ -1,9 +1,5 @@
 // Copyright (c) 2023 8th Wall, Inc.
 
-// app.js is the main entry point for your 8th Wall web app. Code here will execute after head.html
-// is loaded, and before body.html is loaded.
-import './index.css'
-
 import {recenterComponent} from './components/recenter'
 AFRAME.registerComponent('recenter', recenterComponent)
 
