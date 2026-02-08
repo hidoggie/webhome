09b7d3bb966cf69fe13b3d0a67f5ffdcf4050de2
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-09 00:13:24 +0900

    Add files via upload

--- three-sky/sky-scene-pipeline-module.js
+++ three-sky/sky-scene-pipeline-module.js
@@ -1,7 +1,7 @@
 // Returns a pipeline module that initializes a sky scene with models and textures
 // along with simple interactivity and debug options.
 import * as THREE from './../xr_st/r123-three-min.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export const skySampleScenePipelineModule = () => {
   const textureLoader = new THREE.TextureLoader();
