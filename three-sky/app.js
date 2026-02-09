9d19b08567802f73ac14ccd2fab801e0d1e39684
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-09 10:12:09 +0900

    Add files via upload

--- three-sky/app.js
+++ three-sky/app.js
@@ -3,7 +3,7 @@
 // app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
 // is loaded, and before body.html is loaded.
 
-import {skySampleScenePipelineModule} from './sky-scene-pipeline-module.js'
+// import {skySampleScenePipelineModule} from './sky-scene-pipeline-module.js'
 
 const onxrloaded = () => {
   XR8.addCameraPipelineModules([  // Add camera pipeline modules.
