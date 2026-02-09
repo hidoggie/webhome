92a76f2de07bacb8cf738f3fa2c0ae3dbf8f7de5
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-09 11:03:22 +0900

    Add files via upload

--- three-sky/sky-scene-pipeline-module.js
+++ three-sky/sky-scene-pipeline-module.js
@@ -1,6 +1,3 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js'; 
-import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
-
 export const skySampleScenePipelineModule = () => {
   const textureLoader = new THREE.TextureLoader();
   
