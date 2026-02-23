4005e89bb75a94a0e40e56403d36c0527f70b2b0
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-23 09:13:39 +0900

    Add files via upload

---
+++ swap-camera/app.js
@@ -0,0 +1,13 @@
+import {swapDofComponent, swapCameraComponent} from './swap-components.js'
+
+AFRAME.registerComponent('swap-dof', swapDofComponent)
+AFRAME.registerComponent('swap-camera', swapCameraComponent)
+
+const onxrloaded = () => {
+  window.XR8.addCameraPipelineModule({
+    name: 'request-gyro',
+    requiredPermissions: () => ([XR8.XrPermissions.permissions().DEVICE_ORIENTATION]),
+  })
+}
+
+window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
