b0d4811ae7d846e754c76e3d4efbd6ed92fcfe5e
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-25 11:40:16 +0900

    Add files via upload

---
+++ hologram/app.js
@@ -0,0 +1,3 @@
+import {hologram4dsComponent, hologram4dsPrimitive} from './hologram-4ds.js'
+AFRAME.registerComponent('holo4ds', hologram4dsComponent())
+AFRAME.registerPrimitive('hologram-4ds', hologram4dsPrimitive())
