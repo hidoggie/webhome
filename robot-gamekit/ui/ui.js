0ed7e161c371df4ff04d70c00ab7f54bdc2180b3
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-02-27 14:00:45 +0900

    Add files via upload

---
+++ robot-gamekit/ui/ui.js
@@ -0,0 +1,56 @@
+import './index.css'
+const uiHTML = require('./ui.html')
+import {UIElement, Button, Bar, Label, IconBar, Icon} from 'ui-kit'
+
+const powerUpImageOn = require('../assets/img/powerup.png')
+const powerUpImageOff = require('../assets/img/powerup--consumed.png')
+
+const energyIconImage = require('../assets/img/energybar--icon.png')
+
+class UI {
+  init(gameState) {
+    document.getElementById('uiHolder').innerHTML = uiHTML
+
+    this.btnPlay = new Button('#btnPlay').onClick(() => this.onBtnPlayClick())
+    this.btnPause = new Button('#btnPause').onClick(() => this.onBtnPauseClick())
+    this.btnRestart = new Button('#btnRestart').onClick(() => this.onBtnRestartClick())
+
+    this.gameOver_card = new UIElement('#gameOver_card')
+
+    this.energyBar = new Bar('#energyBarProgress')
+    this.scoreLabel = new Label('#scoreLabel')
+    this.scoreEndedLabel = new Label('#gameOverScore')
+    this.powerUpsIcons = new IconBar('#powerUps', powerUpImageOn, gameState.powerUps, 'powerups__item').setInactiveImage(powerUpImageOff)
+
+    this.energyIcon = new Icon('#energyIcon').set(energyIconImage)
+  }
+
+  setBtnPlayVisible(val) {
+    if (this.btnPlay) this.btnPlay.setVisible(val)
+  }
+
+  setBtnPauseVisible(val) {
+    if (this.btnPause) this.btnPause.setVisible(val)
+  }
+
+  setGameOverVisible(val) {
+    if (this.gameOver_card) this.gameOver_card.setVisible(val)
+  }
+
+  setEnergy(val) {
+    if (this.energyBar) this.energyBar.set(val)
+  }
+
+  setScore(val) {
+    if (this.scoreLabel) this.scoreLabel.set(val)
+    if (this.scoreEndedLabel) this.scoreEndedLabel.set(val)
+  }
+
+  setPowerUps(val) {
+    if (this.powerUpsIcons) this.powerUpsIcons.set(val)
+  }
+}
+export {
+  UI,
+}
+
