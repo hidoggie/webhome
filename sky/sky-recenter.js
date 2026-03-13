05bf943f0a7f1ab9c786dc680f9144d14a7b4235
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-03-14 00:35:19 +0900

    Add files via upload

--- sky/sky-recenter.js
+++ sky/sky-recenter.js
@@ -8,3 +8,20 @@ AFRAME.registerComponent('sky-recenter', {
     this.el.addEventListener('sky-coaching-overlay.hide', recenter)
   },
 })
+
+
+AFRAME.registerComponent('hide-show', {
+  init() {
+    const model = document.getElementById('model')
+
+    // ✅ 하늘 인식 성공 → 리센터 + 모델 표시
+    this.el.addEventListener('sky-coaching-overlay.hide', () => {
+      if (model) model.setAttribute('visible', true)
+    })
+
+    // ✅ 하늘 이탈 → 모델 숨김
+    this.el.addEventListener('sky-coaching-overlay.show', () => {
+      if (model) model.setAttribute('visible', false)
+    })
+  },
+})
