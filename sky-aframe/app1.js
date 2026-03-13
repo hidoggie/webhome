fb8a218f4a345ba4a9275f3d24e7d3ffa3980343
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-03-14 01:31:53 +0900

    Add files via upload

---
+++ sky-aframe/app1.js
@@ -0,0 +1,14 @@
+import {ShowHideComponent} from './components/sky-show-hide.js'
+AFRAME.registerComponent('sky-show-hide', ShowHideComponent)
+
+import {debugComponent} from './components/sky-debug.js'
+AFRAME.registerComponent('sky-debug', debugComponent)
+
+import {recenterComponent} from './components/sky-recenter.js'
+AFRAME.registerComponent('sky-recenter', recenterComponent)
+
+//import {skyRemoteAuthoringComponent} from './components/sky-remote-authoring.js'
+//AFRAME.registerComponent('sky-remote-authoring', skyRemoteAuthoringComponent)
+
+//import {deviceOrientationComponent} from './components/device-orientation.js'
+//AFRAME.registerComponent('device-orientation', deviceOrientationComponent)
