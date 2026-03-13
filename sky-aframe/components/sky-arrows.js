35fee7fc5fc602b6458966b32f2efa8b1a60a197
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-03-13 22:41:23 +0900

    Add files via upload

--- sky-aframe/components/sky-arrows.js
+++ sky-aframe/components/sky-arrows.js
@@ -10,10 +10,11 @@ const arrowsComponent = {
     const bottomBar = document.getElementById('bottomBar')
     const rightButton = document.getElementById('rightButton')
     const leftButton = document.getElementById('leftButton')
+    const DotyM = document.getElementById('dotyModel')
 
     scene.addEventListener('sky-coaching-overlay.hide', () => {
       bottomBar.style.display = 'flex'
-      model.setAttribute('visible', true)
+      DotyM.setAttribute('visible', true)
     })
 
     // The current value of the pivot's 'rotation' attribute
