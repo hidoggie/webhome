4d6674b67d0d228dff8e9eb38b0081d74a802ed1
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-06-22 17:16:33 +0900

    Add files via upload

--- lighthouse/js/index_spa.js
+++ lighthouse/js/index_spa.js
@@ -2,7 +2,7 @@
 let map = null;
 let myLocationMarker = null;
 let isMapZoomedIn = false;
-const initialCenter = new naver.maps.LatLng(35.5619, 128.0736);
+const initialCenter = new naver.maps.LatLng(35.6970, 127.9170);
 const initialZoom = 7;
 
 // 10개 이벤트 지역 배열 (전역)
@@ -20,8 +20,8 @@ const eventLocations = [
   },
   {
     title: "영도등대",
-    lat: 35.0522,
-    lng: 129.0840,
+    lat: 35.0808,
+    lng: 129.0212,
     img: "img/youngdo_icon_240.png",
     stamp: "img/stamp-youngdo.png",
     desc: "바다를 디자인하다, 명승지 안의 명소",
