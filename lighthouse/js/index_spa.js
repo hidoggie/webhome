a7e60d20c974a800ef1baea8595feb5fb2326428
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-06-22 16:28:42 +0900

    Add files via upload

--- lighthouse/js/index_spa.js
+++ lighthouse/js/index_spa.js
@@ -2,7 +2,7 @@
 let map = null;
 let myLocationMarker = null;
 let isMapZoomedIn = false;
-const initialCenter = new naver.maps.LatLng(35.8780, 127.6704);
+const initialCenter = new naver.maps.LatLng(35.6956, 127.6670);
 const initialZoom = 7;
 
 // 10개 이벤트 지역 배열 (전역)
