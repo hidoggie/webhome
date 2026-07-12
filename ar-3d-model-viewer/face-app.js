d168542a82bdcc2cd6428ace0e5b9a80f7d01ca6
hidoggie <58835696+hidoggie@users.noreply.github.com>
2026-07-12 16:23:04 +0900

    Add files via upload

---
+++ ar-3d-model-viewer/face-app.js
@@ -0,0 +1,5 @@
+import {changeSceneComponent} from './components/change-scene.js'
+AFRAME.registerComponent('change-view', changeSceneComponent)
+
+import {changeFaceTextureComponent} from './components/face-change.js'
+AFRAME.registerComponent('change-face-texture', changeFaceTextureComponent)
